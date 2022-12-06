785565fcb95aca5a3529b7d48ab2b475e9a17740
Joshuayangg <josh.ym3@gmail.com>
2022-12-06 08:46:17 -0800

    updated prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration)
 const generateAction = async (req, res) => {
   // I build Prompt #2.
   const input = `
-  Give me a detailed seven step plan with links (wrapped in html <a> tags) to free online educational material to completely master the skill defined below, all wrapped in an html p tag.
+  Give me a detailed seven step plan with links (wrapped in html <a> tags) to highly reviewed and reputable free online educational material to completely master the skill defined below, all wrapped in an html p tag. Start from the basics and move towards more advanced topics.
 
   Skill: ${req.body.userInput}
 
@@ -18,7 +18,7 @@ const generateAction = async (req, res) => {
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${input}`,
-    temperature: 0.85,
+    temperature: 0.83,
     max_tokens: 1000,
   })
 
